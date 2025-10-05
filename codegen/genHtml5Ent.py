399fb538823ccc36d23118995541c800d7242847
An-E IOSI <bazikdev@protonmail.com>
2025-10-05 03:52:02 +0300

    remove (c)make and tidyup .c files

--- codegen/genHtml5Ent.py
+++ codegen/genHtml5Ent.py
@@ -23,44 +23,51 @@ from dataclasses import dataclass
 #                 subtable
 
 try:
-    with open('entities.json') as json_data:
-        ents = json.load(json_data)
+	with open("entities.json") as json_data:
+		ents = json.load(json_data)
 except FileNotFoundError:
-    print('entities.json not found, try curl -LJO',
-          'https://html.spec.whatwg.org/entities.json')
-    sys.exit(1)
+	print(
+		"entities.json not found, try curl -LJO",
+		"https://html.spec.whatwg.org/entities.json",
+	)
+	sys.exit(1)
+
 
 def to_cchars(s):
-    r = []
+	r = []
 
-    for c in s.encode():
-        if c >= 0x20 and c <= 0x7E and c != ord("'") and c != ord('\\'):
-            v = f"'{chr(c)}'"
-        else:
-            v = c
-        r += [ v ]
+	for c in s.encode():
+		if c >= 0x20 and c <= 0x7E and c != ord("'") and c != ord("\\"):
+			v = f"'{chr(c)}'"
+		else:
+			v = c
+		r += [v]
 
-    return r
+	return r
+
 
 @dataclass
 class PrefixStackEntry:
-    prefix: str
-    table_id: int
+	prefix: str
+	table_id: int
+
 
 @dataclass
 class AlphaFixup:
-    table_id: int
-    char: int
+	table_id: int
+	char: int
+
 
 @dataclass
 class StringFixup:
-    table_id: int
-    string_index: int
-    super_table_id: int
-    super_offset: int
+	table_id: int
+	string_index: int
+	super_table_id: int
+	super_offset: int
+
 
 # Remove entity strings without trailing semicolon
-keys = (key for key in ents.keys() if key.endswith(';'))
+keys = (key for key in ents.keys() if key.endswith(";"))
 
 # Sort entity strings
 keys = sorted(keys, key=lambda k: k[1:-1])
@@ -71,100 +78,115 @@ prefix_stack = []
 alpha_fixups = []
 string_fixups = []
 for i in range(64):
-    tables.append([])
+	tables.append([])
 
 for i, key in enumerate(keys):
-    name = key[1:-1]
+	name = key[1:-1]
 
-    next_name = None
-    if i + 1 < len(keys):
-        next_name = keys[i+1][1:-1]
+	next_name = None
+	if i + 1 < len(keys):
+		next_name = keys[i + 1][1:-1]
 
-    while prefix_stack and not name.startswith(prefix_stack[-1].prefix):
-        prefix_stack.pop()
+	while prefix_stack and not name.startswith(prefix_stack[-1].prefix):
+		prefix_stack.pop()
 
-    # First character is initial prefix
-    if not prefix_stack:
-        table_id = len(tables)
-        tables.append([])
+	# First character is initial prefix
+	if not prefix_stack:
+		table_id = len(tables)
+		tables.append([])
 
-        prefix_stack.append(PrefixStackEntry(name[0], table_id))
-        alpha_fixups.append(AlphaFixup(table_id, ord(name[0]) % 64))
+		prefix_stack.append(PrefixStackEntry(name[0], table_id))
+		alpha_fixups.append(AlphaFixup(table_id, ord(name[0]) % 64))
 
-    string_index = len(strings)
-    table = tables[prefix_stack[-1].table_id]
-    table_index = len(table)
-    table.append(string_index)
+	string_index = len(strings)
+	table = tables[prefix_stack[-1].table_id]
+	table_index = len(table)
+	table.append(string_index)
 
-    name_offset = len(prefix_stack[-1].prefix)
-    name_chars = to_cchars(name[name_offset:])
-    repl_chars = to_cchars(ents[key]['characters'])
-    semicolon_flag = 0
-    if key[:-1] in ents:
-        semicolon_flag = 0x80
+	name_offset = len(prefix_stack[-1].prefix)
+	name_chars = to_cchars(name[name_offset:])
+	repl_chars = to_cchars(ents[key]["characters"])
+	semicolon_flag = 0
+	if key[:-1] in ents:
+		semicolon_flag = 0x80
 
-    if next_name and next_name.startswith(name):
-        # Create subtable
+	if next_name and next_name.startswith(name):
+		# Create subtable
 
-        strings += [
-            len(name_chars) | semicolon_flag | 0x40, *name_chars,
-            0, 0, # subtable position, to be fixed up
-            len(repl_chars), *repl_chars,
-        ]
+		strings += [
+			len(name_chars) | semicolon_flag | 0x40,
+			*name_chars,
+			0,
+			0,  # subtable position, to be fixed up
+			len(repl_chars),
+			*repl_chars,
+		]
 
-        table_id = len(tables)
-        tables.append([])
+		table_id = len(tables)
+		tables.append([])
 
-        fixup_index = string_index + 1 + len(name_chars)
-        string_fixups.append(StringFixup(
-            table_id, fixup_index, prefix_stack[-1].table_id, table_index,
-        ))
+		fixup_index = string_index + 1 + len(name_chars)
+		string_fixups.append(
+			StringFixup(
+				table_id,
+				fixup_index,
+				prefix_stack[-1].table_id,
+				table_index,
+			)
+		)
 
-        prefix_stack.append(PrefixStackEntry(name, table_id))
-    else:
-        strings += [
-            len(name_chars) | semicolon_flag, *name_chars,
-            len(repl_chars), *repl_chars,
-        ]
+		prefix_stack.append(PrefixStackEntry(name, table_id))
+	else:
+		strings += [
+			len(name_chars) | semicolon_flag,
+			*name_chars,
+			len(repl_chars),
+			*repl_chars,
+		]
 
 # Concat tables and record ranges
-ranges = [ 0 ]
+ranges = [0]
 values = []
 for table in tables:
-    values += table
-    ranges.append(len(values))
+	values += table
+	ranges.append(len(values))
 
 # Create alpha table
-alpha = [ 0 ] * (59 * 3)
+alpha = [0] * (59 * 3)
 for fixup in alpha_fixups:
-    table_id, c = fixup.table_id, fixup.char
-    start = ranges[table_id]
-    end = ranges[table_id+1]
-    alpha[c*3:c*3+3] = [ start & 0xFF, start >> 8, end - start ]
+	table_id, c = fixup.table_id, fixup.char
+	start = ranges[table_id]
+	end = ranges[table_id + 1]
+	alpha[c * 3 : c * 3 + 3] = [start & 0xFF, start >> 8, end - start]
 
 # Fix up subtable positions
 for fixup in string_fixups:
-    table_id, i = fixup.table_id, fixup.string_index
-    start = ranges[table_id]
-    end = ranges[table_id+1]
-    super_index = ranges[fixup.super_table_id] + fixup.super_offset
-    strings[i:i+2] = [ start - super_index, end - start ]
+	table_id, i = fixup.table_id, fixup.string_index
+	start = ranges[table_id]
+	end = ranges[table_id + 1]
+	super_index = ranges[fixup.super_table_id] + fixup.super_offset
+	strings[i : i + 2] = [start - super_index, end - start]
 
 # Print tables
 
+
 def gen_table(ctype, cname, values, fmt, elems_per_line):
-    count = len(values)
-    r = ''
+	count = len(values)
+	r = ""
 
-    for i in range(count):
-        if i != 0: r += ','
-        if i % elems_per_line == 0: r += '\n    '
-        else: r += ' '
-        r += fmt % values[i]
+	for i in range(count):
+		if i != 0:
+			r += ","
+		if i % elems_per_line == 0:
+			r += "\n    "
+		else:
+			r += " "
+		r += fmt % values[i]
 
-    return f'static const {ctype} {cname}[{count}] = {{{r}\n}};\n\n'
+	return f"static const {ctype} {cname}[{count}] = {{{r}\n}};\n\n"
 
-with open('codegen/html5ent.inc', 'w') as out:
-    out.write(gen_table('unsigned char', 'htmlEntAlpha', alpha, '%3d', 15))
-    out.write(gen_table('unsigned short', 'htmlEntValues', values, '%5d', 10))
-    out.write(gen_table('unsigned char', 'htmlEntStrings', strings, '%3s', 15))
+
+with open("codegen/html5ent.inc", "w") as out:
+	out.write(gen_table("unsigned char", "htmlEntAlpha", alpha, "%3d", 15))
+	out.write(gen_table("unsigned short", "htmlEntValues", values, "%5d", 10))
+	out.write(gen_table("unsigned char", "htmlEntStrings", strings, "%3s", 15))
--- codegen/genUnicode.py
+++ codegen/genUnicode.py
@@ -5,8 +5,8 @@
 # current Unicode information (Version 4.0.1)
 #
 # NOTE: there is an 'alias' facility for blocks which are not present in
-#	the current release, but are needed for ABI compatibility.  This
-#	must be accomplished MANUALLY!  Please see the comments below under
+# the current release, but are needed for ABI compatibility.  This
+# must be accomplished MANUALLY!  Please see the comments below under
 #     'blockAliases'
 #
 import sys
@@ -20,8 +20,10 @@ import rangetab
 blockAliases = []
 blockAliases.append("CombiningMarksforSymbols:CombiningDiacriticalMarksforSymbols")
 blockAliases.append("Greek:GreekandCoptic")
-blockAliases.append("PrivateUse:PrivateUseArea,SupplementaryPrivateUseArea-A," + 
-	"SupplementaryPrivateUseArea-B")
+blockAliases.append(
+	"PrivateUse:PrivateUseArea,SupplementaryPrivateUseArea-A,"
+	+ "SupplementaryPrivateUseArea-B"
+)
 
 # minTableSize gives the minimum number of ranges which must be present
 # before a range table is produced.  If there are less than this
@@ -39,47 +41,47 @@ catfile = "UnicodeData-4.0.1.txt"
 #
 BlockNames = {}
 try:
-    blocks = open(blockfile, "r")
+	blocks = open(blockfile, "r")
 except:
-    print("Missing %s, aborting ..." % blockfile)
-    sys.exit(1)
+	print("Missing %s, aborting ..." % blockfile)
+	sys.exit(1)
 
 for line in blocks.readlines():
-    if line[0] == '#':
-        continue
-    line = line.strip()
-    if line == '':
-        continue
-    try:
-        fields = line.split(';')
-        range = fields[0].strip()
-        (start, end) = range.split("..")
-        name = fields[1].strip()
-        name = name.replace(' ', '')
-    except:
-        print("Failed to process line: %s" % (line))
-        continue
-    start = int(start, 16)
-    end = int(end, 16)
-    try:
-        BlockNames[name].append((start, end))
-    except:
-        BlockNames[name] = [(start, end)]
+	if line[0] == "#":
+		continue
+	line = line.strip()
+	if line == "":
+		continue
+	try:
+		fields = line.split(";")
+		range = fields[0].strip()
+		(start, end) = range.split("..")
+		name = fields[1].strip()
+		name = name.replace(" ", "")
+	except:
+		print("Failed to process line: %s" % (line))
+		continue
+	start = int(start, 16)
+	end = int(end, 16)
+	try:
+		BlockNames[name].append((start, end))
+	except:
+		BlockNames[name] = [(start, end)]
 blocks.close()
 print("Parsed %d blocks descriptions" % (len(BlockNames.keys())))
 
 for block in blockAliases:
-    alias = block.split(':')
-    alist = alias[1].split(',')
-    for comp in alist:
-        if comp in BlockNames:
-            if alias[0] not in BlockNames:
-                BlockNames[alias[0]] = []
-            for r in BlockNames[comp]:
-                BlockNames[alias[0]].append(r)
-        else:
-            print("Alias %s: %s not in Blocks" % (alias[0], comp))
-            continue
+	alias = block.split(":")
+	alist = alias[1].split(",")
+	for comp in alist:
+		if comp in BlockNames:
+			if alias[0] not in BlockNames:
+				BlockNames[alias[0]] = []
+			for r in BlockNames[comp]:
+				BlockNames[alias[0]].append(r)
+		else:
+			print("Alias %s: %s not in Blocks" % (alias[0], comp))
+			continue
 
 #
 # Next process the Categories file. This is more complex, since
@@ -92,54 +94,54 @@ for block in blockAliases:
 # (2-character) name are entered into this dictionary.
 #
 try:
-    data = open(catfile, "r")
+	data = open(catfile, "r")
 except:
-    print("Missing %s, aborting ..." % catfile)
-    sys.exit(1)
+	print("Missing %s, aborting ..." % catfile)
+	sys.exit(1)
 
-nbchar = 0;
+nbchar = 0
 Categories = {}
 for line in data.readlines():
-    if line[0] == '#':
-        continue
-    line = line.strip()
-    if line == '':
-        continue
-    try:
-        fields = line.split(';')
-        point = fields[0].strip()
-        value = 0
-        while point != '':
-            value = value * 16
-            if point[0] >= '0' and point[0] <= '9':
-                value = value + ord(point[0]) - ord('0')
-            elif point[0] >= 'A' and point[0] <= 'F':
-                value = value + 10 + ord(point[0]) - ord('A')
-            elif point[0] >= 'a' and point[0] <= 'f':
-                value = value + 10 + ord(point[0]) - ord('a')
-            point = point[1:]
-        name = fields[2]
-    except:
-        print("Failed to process line: %s" % (line))
-        continue
-    
-    nbchar = nbchar + 1
-    # update entry for "full name"
-    try:
-        Categories[name].append(value)
-    except:
-        try:
-            Categories[name] = [value]
-        except:
-            print("Failed to process line: %s" % (line))
-    # update "general category" name
-    try:
-        Categories[name[0]].append(value)
-    except:
-        try:
-            Categories[name[0]] = [value]
-        except:
-            print("Failed to process line: %s" % (line))
+	if line[0] == "#":
+		continue
+	line = line.strip()
+	if line == "":
+		continue
+	try:
+		fields = line.split(";")
+		point = fields[0].strip()
+		value = 0
+		while point != "":
+			value = value * 16
+			if point[0] >= "0" and point[0] <= "9":
+				value = value + ord(point[0]) - ord("0")
+			elif point[0] >= "A" and point[0] <= "F":
+				value = value + 10 + ord(point[0]) - ord("A")
+			elif point[0] >= "a" and point[0] <= "f":
+				value = value + 10 + ord(point[0]) - ord("a")
+			point = point[1:]
+		name = fields[2]
+	except:
+		print("Failed to process line: %s" % (line))
+		continue
+
+	nbchar = nbchar + 1
+	# update entry for "full name"
+	try:
+		Categories[name].append(value)
+	except:
+		try:
+			Categories[name] = [value]
+		except:
+			print("Failed to process line: %s" % (line))
+	# update "general category" name
+	try:
+		Categories[name[0]].append(value)
+	except:
+		try:
+			Categories[name[0]] = [value]
+		except:
+			print("Failed to process line: %s" % (line))
 
 data.close()
 print("Parsed %d char generating %d categories" % (nbchar, len(Categories.keys())))
@@ -149,34 +151,34 @@ print("Parsed %d char generating %d categories" % (nbchar, len(Categories.keys()
 #
 # reduce the number list into ranges
 for cat in Categories.keys():
-    list = Categories[cat]
-    start = -1
-    prev = -1
-    end = -1
-    ranges = []
-    for val in list:
-        if start == -1:
-            start = val
-            prev = val
-            continue
-        elif val == prev + 1:
-            prev = val
-            continue
-        elif prev == start:
-            ranges.append((prev, prev))
-            start = val
-            prev = val
-            continue
-        else:
-            ranges.append((start, prev))
-            start = val
-            prev = val
-            continue
-    if prev == start:
-        ranges.append((prev, prev))
-    else:
-        ranges.append((start, prev))
-    Categories[cat] = ranges
+	list = Categories[cat]
+	start = -1
+	prev = -1
+	end = -1
+	ranges = []
+	for val in list:
+		if start == -1:
+			start = val
+			prev = val
+			continue
+		elif val == prev + 1:
+			prev = val
+			continue
+		elif prev == start:
+			ranges.append((prev, prev))
+			start = val
+			prev = val
+			continue
+		else:
+			ranges.append((start, prev))
+			start = val
+			prev = val
+			continue
+	if prev == start:
+		ranges.append((prev, prev))
+	else:
+		ranges.append((start, prev))
+	Categories[cat] = ranges
 
 #
 # Assure all data is in alphabetic order, since we will be doing binary
@@ -190,60 +192,57 @@ ckeys = sorted(Categories.keys())
 # Generate the resulting files
 #
 try:
-    output = open("codegen/unicode.inc", "w")
+	output = open("codegen/unicode.inc", "w")
 except:
-    print("Failed to open codegen/unicode.inc")
-    sys.exit(1)
+	print("Failed to open codegen/unicode.inc")
+	sys.exit(1)
 
 #
 # For any categories with more than minTableSize ranges we generate
 # a range table suitable for xmlCharInRange
 #
 for name in ckeys:
-    if len(Categories[name]) <= minTableSize or name == 'Cs':
-        continue
-    ranges = Categories[name]
-    group = rangetab.gen_range_tables(output, 'xml' + name, 'S', 'L', ranges)
-    output.write("static const xmlChRangeGroup xml%sG = %s;\n\n" %
-                 (name, group))
+	if len(Categories[name]) <= minTableSize or name == "Cs":
+		continue
+	ranges = Categories[name]
+	group = rangetab.gen_range_tables(output, "xml" + name, "S", "L", ranges)
+	output.write("static const xmlChRangeGroup xml%sG = %s;\n\n" % (name, group))
 
 for name in ckeys:
-    if name == 'Cs':
-        continue
-    ranges = Categories[name]
-    output.write("static int\nxmlUCSIsCat%s(int code) {\n" % name)
-    if len(Categories[name]) > minTableSize:
-        output.write("    return(xmlCharInRange((unsigned int)code, &xml%sG)"
-            % name)
-    else:
-        start = 1
-        for range in ranges:
-            (begin, end) = range;
-            if start:
-                output.write("    return(");
-                start = 0
-            else:
-                output.write(" ||\n           ");
-            if (begin == end):
-                output.write("(code == %s)" % (hex(begin)))
-            else:
-                output.write("((code >= %s) && (code <= %s))" % (
-                         hex(begin), hex(end)))
-    output.write(");\n}\n\n")
+	if name == "Cs":
+		continue
+	ranges = Categories[name]
+	output.write("static int\nxmlUCSIsCat%s(int code) {\n" % name)
+	if len(Categories[name]) > minTableSize:
+		output.write("    return(xmlCharInRange((unsigned int)code, &xml%sG)" % name)
+	else:
+		start = 1
+		for range in ranges:
+			(begin, end) = range
+			if start:
+				output.write("    return(")
+				start = 0
+			else:
+				output.write(" ||\n           ")
+			if begin == end:
+				output.write("(code == %s)" % (hex(begin)))
+			else:
+				output.write("((code >= %s) && (code <= %s))" % (hex(begin), hex(end)))
+	output.write(");\n}\n\n")
 
 #
 # Range tables for blocks
 #
 
-blockGroups = ''
+blockGroups = ""
 for block in bkeys:
-    name = block.replace('-', '')
-    ranges = BlockNames[block]
-    group = rangetab.gen_range_tables(output, 'xml' + name, 'S', 'L', ranges)
-    output.write("\n")
-    if blockGroups != '':
-        blockGroups += ",\n"
-    blockGroups += '  {"%s",\n   %s}' % (block, group)
+	name = block.replace("-", "")
+	ranges = BlockNames[block]
+	group = rangetab.gen_range_tables(output, "xml" + name, "S", "L", ranges)
+	output.write("\n")
+	if blockGroups != "":
+		blockGroups += ",\n"
+	blockGroups += '  {"%s",\n   %s}' % (block, group)
 
 output.write("static const xmlUnicodeRange xmlUnicodeBlocks[] = {\n")
 output.write(blockGroups)
